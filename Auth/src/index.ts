e90b800b9c7b4460a6a37dd99ae2ccc33431e4b6
taimoor954 <taimoormuhammad954@gmail.com>
2021-03-29 17:58:21 +0500

    skaffold bug solved

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express'
 
 const app = express()
-// app.use(
-//   express.json({
-//     limit: '10kb', //size of req.body can be upto 10kb
-//   })
-// ); //BODY PARSER
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//     limit: '10kb',
-//   }) 
-// );
+app.use(
+  express.json({
+    limit: '10kb', //size of req.body can be upto 10kb
+  })
+); //BODY PARSER
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: '10kb',
+  }) 
+);
 app.listen(3000, ()=>{
   console.log('Auth Running at port 3000');
   
