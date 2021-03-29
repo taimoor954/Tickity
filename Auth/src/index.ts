512224fa30e0241913a466f2076eefd82c083249
taimoor954 <taimoormuhammad954@gmail.com>
2021-03-29 16:50:03 +0500

    skaffold dev added and working for auto crearting image

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,8 +1,17 @@
 import express from 'express'
-import {json} from 'body-parser'
 
 const app = express()
-app.use(json())
+// app.use(
+//   express.json({
+//     limit: '10kb', //size of req.body can be upto 10kb
+//   })
+// ); //BODY PARSER
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//     limit: '10kb',
+//   }) 
+// );
 app.listen(3000, ()=>{
   console.log('Auth Running at port 3000');
   
