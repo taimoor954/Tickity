eaf4fcd8565a00827fa2a0c3b9ac1557535bfff8
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-04 14:59:49 +0500

    auth srv conntected with auth-monog service succesfully...

--- Auth/src/index.ts
+++ Auth/src/index.ts
@@ -1,13 +1,14 @@
-import express from 'express'
-import 'express-async-errors' //USING THIS PACKAGE WE CAN DEAL WITH ASYNC ERRORS IN SYNC WAY WITHIUT USING NEXT MIDDLEWARE
-import { currentUserRouter } from './Routes/current-user'
-import { signinRouter } from './Routes/signin'
-import { signoutRouter } from './Routes/signout'
-import { signupRouter } from './Routes/signup'
-import {errorHandler} from '../src/middlewares/error-handler'
-import {NotFoundError} from './Errors/Not-Found-Error'
+import express from 'express';
+import mongoose from 'mongoose';
+import 'express-async-errors'; //USING THIS PACKAGE WE CAN DEAL WITH ASYNC ERRORS IN SYNC WAY WITHIUT USING NEXT MIDDLEWARE
+import { currentUserRouter } from './Routes/current-user';
+import { signinRouter } from './Routes/signin';
+import { signoutRouter } from './Routes/signout';
+import { signupRouter } from './Routes/signup';
+import { errorHandler } from '../src/middlewares/error-handler';
+import { NotFoundError } from './Errors/Not-Found-Error';
 
-const app = express()
+const app = express();
 app.use(
   express.json({
     limit: '10kb', //size of req.body can be upto 10kb
@@ -21,22 +22,38 @@ app.use(
 );
 
 //ROUTE MIDDLEWARE
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
 
 //Route not Found
-app.all('*',async (req, res)=>{
-  throw new NotFoundError()
-})
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 //GLOBAL ERROR HANDLE MIDDLEWARE
-app.use(errorHandler)
+app.use(errorHandler);
+
+//MONGO STARTUP
+const mongoConnection = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to monogo instance succesfully....');
+    
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+mongoConnection()
 
 app.listen(3000, () => {
   console.log('Auth Running at port 3000');
-
-})
+});
 
 //ASYNC ERRROS ARE THOSE ERRORS JO ASYNC HANDLER KAY ANDER HO
