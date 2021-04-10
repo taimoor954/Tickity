353c73086603d47d19791b1199f4e8c9f2c46f52
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-10 18:40:26 +0500

    done with sign in route

--- Auth/src/Routes/signin.ts
+++ Auth/src/Routes/signin.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../Errors/BadRequest';
 import { RequestValidationError } from '../Errors/req-validation';
+import  jwt  from "jsonwebtoken";
 import { requestValidator } from '../middlewares/validate-request';
-
+import {User} from '../Models/userModel'
+import { Password } from '../Services/password';
 const router = express.Router();
 //SIGN IN ALGO
 
@@ -23,9 +26,29 @@ router.post(
       .withMessage('Must provide a password'),
   ],
   requestValidator,
-  (request: Request, response: Response) => {
-    
-    response.send('hi there from sign in');
+  async (request: Request, response: Response) => {
+    const {email, password} = request.body
+    const existingUser = await User.findOne({email})
+    if(!existingUser){
+      throw new BadRequestError('Something wrong with email or password')
+      return
+    }
+    const passwordMatch = await Password.comparePassword(existingUser.password, password)
+    if(!passwordMatch){
+      throw new BadRequestError('Something wrong with email or password')
+    } 
+    const userJwt = jwt.sign({ id: existingUser.id, email: existingUser.email }, process.env.jwt!) //process.env.jwt! here "!" mean hey ts, process.env.jwt is defined and we are 100% sure about that. if we dont put "!" it will show error
+    //store it into session object 
+    request.session = {
+      jwt: userJwt
+    }
+
+
+    return response.status(201).json({
+      status: 'successfuly login user',
+      data: existingUser
+    })
+
   }
 );
 
