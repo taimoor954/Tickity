010e21fb411755ccccb116ab7fef60160b284a78
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-11 17:54:39 +0500

    current user and require auth working fine

--- Auth/src/Routes/current-user.ts
+++ Auth/src/Routes/current-user.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
+import { requireAuth } from '../middlewares/require-auth';
 const router = express.Router();
-router.get('/api/users/currentuser', currentUser, (request, response) => {
+router.get('/api/users/currentuser', currentUser, requireAuth, (request, response) => {
   response.status(200).json({
     status: 'Success',
     data: request.currentUser || null, //if no user is signed in then send null
--- Auth/src/middlewares/require-auth.ts
+++ Auth/src/middlewares/require-auth.ts
@@ -2,13 +2,11 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { NotAuthorizeError } from '../Errors/Not-Authorize-error';
 
-export const requireAuth = (
-  request: Request,
+export const requireAuth = (request: Request,
   response: Response,
-  next: NextFunction
-) => {
+  next: NextFunction) => {
   //user access route if they do not have access to  it
-  if (request.currentUser) {
+  if (!request.currentUser) {
     throw new NotAuthorizeError();
   }
   next()
