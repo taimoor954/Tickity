cbf0d649afd63a5290a4c36985744eb67a8bf641
taimoor954 <taimoormuhammad954@gmail.com>
2021-04-11 17:43:08 +0500

    Error handeling class for not Authozize user has been added. Also require authorize middleware is added

---
+++ Auth/src/Errors/Not-Authorize-error.ts
@@ -0,0 +1,15 @@
+import { CustomError } from "./CustomError-Abstract";
+
+export class NotAuthorizeError extends CustomError{
+  statusCode = 401
+  constructor()
+  {
+    super("Not authorized")
+    Object.setPrototypeOf(this, NotAuthorizeError.prototype)
+  }
+  searalizeErrors()
+  {
+  return [{message:"Not authorized"}]
+
+}
+}
---
+++ Auth/src/middlewares/require-auth.ts
@@ -0,0 +1,16 @@
+//work as protecred middleware to check if user can access the rout onoly if he is log in
+import express, { Request, Response, NextFunction } from 'express';
+import { NotAuthorizeError } from '../Errors/Not-Authorize-error';
+
+export const requireAuth = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  //user access route if they do not have access to  it
+  if (request.currentUser) {
+    throw new NotAuthorizeError();
+  }
+  next()
+
+};
